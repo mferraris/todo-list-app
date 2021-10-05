75b2377bbd6eebeac257d76aca1ad6f1355e4640
mferraris <mgl.ferraris@gmail.com>
2021-10-05 15:05:37 +0200

    created relations between tables

--- models/todoitem.js
+++ models/todoitem.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class todoItem extends Model {
     /**
@@ -10,15 +8,19 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      todoItem.belongsTo(models.todoList);
     }
-  };
-  todoItem.init({
-    task: DataTypes.STRING,
-    deadline: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'todoItem',
-  });
+  }
+  todoItem.init(
+    {
+      task: DataTypes.STRING,
+      deadline: DataTypes.STRING,
+      important: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "todoItem",
+    }
+  );
   return todoItem;
 };
--- models/user.js
+++ models/user.js
@@ -7,8 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
     static associate(models) {
-      // define association here
+      user.hasMany(models.todoList); // ONE user can have MANY to do lists
     }
   }
   user.init(
